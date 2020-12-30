d0bfc49b5ca27898a5097a0938f5904401ee844e
pratyushatmindfire <pratyushp@mindfiresolutions.com>
2020-12-31 01:29:15 +0800

    Added CSS for table side of page, added substantial javascript to fetch form data and display in console

--- js/controlscript.js
+++ js/controlscript.js
@@ -1,1 +1,38 @@
 console.log("Holla!")
+
+function addtoTable()
+{
+	event.preventDefault();
+	var albumname=document.getElementById('name').value;
+	var albumprice=document.getElementById('price').value;
+	var albumdesc=document.getElementById('desc').value;
+	
+
+	var albumgenreNodeList=document.getElementsByName('genre');
+	var albumgenres=[];
+	console.log(albumgenreNodeList);
+
+	var targetAudienceNodeList = document.getElementsByName('targetAudience');
+	var targetAudience=[];
+
+	for(var each of targetAudienceNodeList)
+	{
+		if(each.checked === true)
+		{
+			targetAudience.push(each.value)
+		}
+	}
+
+	for(var each of albumgenreNodeList)
+	{
+		if(each.checked === true)
+		{
+			albumgenres.push(each.value)
+		}
+	}
+
+	targetAudience=targetAudience.toString();
+	albumgenres=albumgenres.toString();
+
+	console.log(albumname, albumprice, albumdesc, targetAudience, albumgenres)
+}
